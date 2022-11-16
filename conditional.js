ae2acc3f5c18477f503f007e70ea9820dc403a72
unknown <omilude.omololamercy@gmail.com>
2022-11-16 04:09:57 +0100

    name

---
+++ conditional.js
@@ -0,0 +1,8 @@
+let firstname ="omolola";
+
+if(firstname == 'omolola'){
+    document.getElementById("name").innerHTML="omolola is the firstname";
+}
+else if(firstname =="omilude"){
+    document.getElementById("wrong").innerHTML=" you presented your surname as firstname"
+}

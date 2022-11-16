9f87717acdc1401b045edf07ac8caccfb2037578
unknown <omilude.omololamercy@gmail.com>
2022-11-16 04:20:46 +0100

    correct

--- conditional.js
+++ conditional.js
@@ -6,3 +6,6 @@ if(firstname == 'omolola'){
 else if(firstname =="omilude"){
     document.getElementById("wrong").innerHTML=" you presented your surname as firstname"
 }
+else if(firstname== "mercy"){
+    document.getElementById("correct").innerHTML="you presented your middle name as first name"
+}
